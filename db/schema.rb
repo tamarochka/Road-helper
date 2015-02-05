ba157179eb0c19210dde385899a9bd470ae4aa24
tamarochka <tamara.zenkevich@gmail.com>
2015-02-04 23:08:31 -0500

    created location model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150205033822) do
+ActiveRecord::Schema.define(version: 20150205035123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "locations", force: :cascade do |t|
+    t.string   "name",       null: false
+    t.string   "address",    null: false
+    t.float    "latitude",   null: false
+    t.float    "longitude",  null: false
+    t.text     "notes"
+    t.integer  "user_id",    null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
