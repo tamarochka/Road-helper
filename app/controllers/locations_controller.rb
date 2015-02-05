7868569e2ca6645a0f1ce34e38daf68f3b634fe1
tamarochka <tamara.zenkevich@gmail.com>
2015-02-04 23:39:06 -0500

    added locations controller and new/index views

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,23 @@
+class LocationsController < ApplicationController
+  def index
+    @locations = Location.all
+  end
+  def new
+    @location = Location.new
+  end
+
+  def create
+    @location = Location.new(location_params)
+    @location.user = current_user
+    if @location.save
+      redirect_to locations_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def location_params
+    params.require(:location).permit(:name, :address, :longtitude, :latitude, :notes)
+  end
+end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,5 @@
 class Location < ActiveRecord::Base
   belongs_to :user
+  geocoded_by :address
+  after_validation :geocode, :if => :address_changed?
 end
