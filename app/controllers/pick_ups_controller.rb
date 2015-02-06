a1cfcb5777422eba562535459b1218fd4245b2e0
tamarochka <tamara.zenkevich@gmail.com>
2015-02-05 21:43:07 -0500

    added new view and controller for pick-ups

---
+++ app/controllers/pick_ups_controller.rb
@@ -0,0 +1,26 @@
+class PickUpsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
+  def index
+    @pick_up = PickUp.all
+  end
+
+  def new
+    @pick_up = PickUp.new
+  end
+
+  def create
+    @pick_up = PickUp.new(pickup_params)
+    @pick_up.user = current_user
+    if @pick_up.save
+      redirect_to pick_ups_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def pickup_params
+    params.require(:pick_up).permit(:location_id, :item, :date, :quantity)
+  end
+end
