0d9d2d4ab9f221717f96c0b7f5b58ecd4c9021f1
tamarochka <tamara.zenkevich@gmail.com>
2015-02-12 21:37:01 -0500

    added show view for customer and controller action

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -22,6 +22,10 @@ class CustomersController < ApplicationController
     end
   end
 
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
   private
   def customer_params
     params.require(:customer).permit(:name, :address, :longtitude, :latitude)
